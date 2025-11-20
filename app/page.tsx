eac99a279bc7429aba4e10f65e5738c2415cf75a
Lukáš Vaněk <lukas.vanek.97@gmail.com>
2025-11-20 15:40:40 +0100

    CMS version

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "../components/Footer";
 export const revalidate = 3600;
 
 async function getPageData() {
-  const res = await fetch(`http://localhost:3000/api/page-data`, {
+  const res = await fetch(`https://sowepro.cz/api/page-data`, {
     next: { revalidate: 3600 }
   });
 
